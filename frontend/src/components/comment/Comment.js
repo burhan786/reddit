9292336b2f19d765b027e717ae14f3fefe618e77
Sagnik Chatterjee <chatterjeesagnik88@gmail.com>
2021-07-24 17:02:20 +0530

    feat: added basics comment and post dirs

---
+++ frontend/src/components/comment/Comment.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./comment.css";
+
+function Comment() {
+  return <div className="comment-section">Sample Comment Section Holder</div>;
+}
+
+export default Comment;
